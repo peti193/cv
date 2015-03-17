72cc2cf84eff4f9e2b2ae81517595fdd22dc246e
peti <peter.kis@penceo.com>
2015-03-17 16:52:44 +0100

    init repo

---
+++ js/main.js
@@ -0,0 +1,24 @@
+/**
+ * Global variables
+ *
+ * @author Jonathan Path
+ */
+
+// Remove NavBar from iOS
+if( !window.location.hash && window.addEventListener ){
+    window.addEventListener( "load",function() {
+        setTimeout(function(){
+            window.scrollTo(0, 0);
+        }, 0);
+    });
+    window.addEventListener( "orientationchange",function() {
+        setTimeout(function(){
+            window.scrollTo(0, 0);
+        }, 0);
+    });
+}
+
+$(document).ready(function(){
+   // var main_height = $(".main-content").height();
+    //$(".aside").height(main_height);
+});
